787d536b8a0a42d8992258726a82aedd3b89337a
FusoLay <scm.tharaphywinaung@gmail.com>
2021-12-14 09:52:23 +0630

    edit

--- resources/views/user/update_user_confirm.blade.php
+++ resources/views/user/update_user_confirm.blade.php
@@ -39,7 +39,7 @@
             </div>
             <div class="text-right mt-5">
               <input type="submit" name="submit" class="btn btn-info" value="Update">
-              <a href="javascript:history.back()" type="button" class="btn btn-outline-info">Cancel</a>
+              <a href="javascript:history.back()" type="button" class="btn btn-outline-info">back</a>
             </div>
           </form>
         </div>
