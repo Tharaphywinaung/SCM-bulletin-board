206a342450340109cabe88f9164ab2882167a2d1
FusoLay <scm.tharaphywinaung@gmail.com>
2021-12-18 13:37:41 +0630

    1st review

--- app/Imports/PostsImport.php
+++ app/Imports/PostsImport.php
@@ -3,25 +3,35 @@
 namespace App\Imports;
 
 use App\Models\Post;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Validator;
 use Auth;
 
-class PostsImport implements ToModel,WithHeadingRow
+class PostsImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new Post([
+        Validator::make($rows->toArray(), [
+            '*.title' => 'required|unique:posts',
+            '*.description' => 'required',
+            '*.status' => 'required',
+        ])->validate();
+
+        foreach ($rows as $row) {
+            Post::create([
             'title'     => $row['title'],
             'description'    => $row['description'], 
             'status' => $row['status'],
             'create_user_id' => Auth::user()->id,
             'updated_user_id' => Auth::user()->id,
-        ]);
+            ]);
+        }
     }
 }
--- resources/views/post/csv_upload.blade.php
+++ resources/views/post/csv_upload.blade.php
@@ -12,15 +12,25 @@
     </div>
     <div class="card-body">
       <form id="excel-csv-import-form" method="POST"  action="{{ url('import-excel-csv-file') }}" accept-charset="utf-8" enctype="multipart/form-data">
-        @csrf   
+        @csrf  
+        @if (count($errors) > 0)
+          <div class="row">
+            <div class="col-md-8 col-md-offset-1">
+              <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Error!</h4>
+                @foreach($errors->all() as $error)
+                  {{ $error }} <br>
+                @endforeach      
+              </div>
+            </div>
+          </div>
+        @endif
         <div class="row">
           <div class="col-md-12">
             <div class="form-group">
               <input type="file" name="file" placeholder="Choose file">
             </div>
-          @error('file')
-            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-          @enderror
           </div>              
           <div class="col-md-12">
             <button type="submit" class="btn btn-info" id="submit">Submit</button>
